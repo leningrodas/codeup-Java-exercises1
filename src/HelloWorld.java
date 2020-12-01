cb5c4c290dd0a79d8d753f23b6cf9d46188ab4ed
lening rodas <lening.a.rodas@gmail.com>
2020-12-01 15:24:08 -0600

    first java exercise complete

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This creates a new line compared to the ones below");
+        System.out.print("Hello, World!");
+
+        System.out.print("Hello, World 2");
+
+    }
+
+}
+
+
