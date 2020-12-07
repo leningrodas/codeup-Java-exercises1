83b28f57eedf77a0b137492d0a590b6e4e810ee9
lening rodas <lening.a.rodas@gmail.com>
2020-12-07 09:27:40 -0600

    method exercises

---
+++ src/MethodExercises.java
@@ -0,0 +1,89 @@
+
+
+public class MethodExercises {
+
+    public static int numSum(int num1, int num2) {
+            return num1 + num2;
+        };
+
+    public static int numSub(int num1, int num2){
+        return num1 - num2;
+    };
+
+    public static int numMulti(int num1, int num2){
+        return num1 * num2;
+    };
+
+
+    public static int numDiv(int num1, int num2){
+        return num1/num2;
+    };
+
+    public static int numModu(int num1, int num2){
+        return num1 % num2;
+    };
+
+    public static int numZero(int num1, int num2){
+        return num1 / num2;
+    };
+
+    public static void getInterger(int min, int max, int userInput) {
+      if(userInput >= min && userInput <= 10){
+          System.out.println("Great you are within range ");
+                } else {
+          System.out.println("you are out of scope. bye felicia");
+      }
+
+    };
+
+
+
+
+
+    public static void main(String[] args){
+
+//Questions 1
+
+
+//        System.out.println(numSum(2,2));
+//
+//        System.out.println(numSub(5,7));
+//
+//        System.out.println(numMulti(5,5));
+//
+//        System.out.println(numDiv(20,5));
+//
+//        System.out.println(numModu(40,7));
+
+//        System.out.println(numZero(7,0));
+
+
+//  Questions 2:
+
+//        Scanner userInput = new Scanner(System.in);
+//
+//        System.out.println("enter number between 1 and 10");
+//            int userNum = userInput.nextInt();
+//            getInterger(1,10, userNum);
+//
+//
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
