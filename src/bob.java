9a73e60ed1eb1b7178ca85d2964c9c59a2f048d0
lening rodas <lening.a.rodas@gmail.com>
2020-12-10 16:36:47 -0600

    array exercise in  progress

--- src/bob.java
+++ src/bob.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
+
 
 public class bob {
     public static void main(String[] args) {
 
-        Scanner userInput = new Scanner(System.in);
-
-        System.out.println("talk to bob");
-        String question = "sure";
-        String endsWithExclam = "Whoa, Chill out! Son,";
-        String dontSay = "Fine. Be that way!";
-        String whatEv = "Whatever.";
-        String empty = userInput.nextLine();
-
-        if (userInput.endsWith("?")) {
-            System.out.println(question);
-        } else if (userInput.endsWith("!")) {
-            System.out.print(endsWithExclam);
-        } else if (userInput.endsWith(empty)) {
-            System.out.println(dontSay);
-        }
+//        Scanner userInput = new Scanner(System.in);
+//
+//        System.out.println("talk to bob");
+//        String question = "sure";
+//        String endsWithExclam = "Whoa, Chill out! Son,";
+//        String dontSay = "Fine. Be that way!";
+//        String whatEv = "Whatever.";
+//        String empty = userInput.nextLine();
+//
+//        if (userInput.endsWith("?")) {
+//            System.out.println(question);
+//        } else if (userInput.endsWith("!")) {
+//            System.out.print(endsWithExclam);
+//        } else if (userInput.endsWith(empty)) {
+//            System.out.println(dontSay);
+//        }
 
 
     }
